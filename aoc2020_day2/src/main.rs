54401e5661287557a35cc6af9cdf8c10a2a99dbd
John Ward <john@johnward.net>
2020-12-02 10:22:17 +0000

    Day2 Part 1 completed

--- aoc2020_day2/src/main.rs
+++ aoc2020_day2/src/main.rs
@@ -6,10 +6,12 @@ fn main()  -> Result<(), Box<dyn std::error::Error>> {
 
     let content = std::fs::read_to_string(&filename)?;
 
+    let mut password_count = 0;
+
     for line in content.lines() {
         let comps: Vec<&str> = line.split(':').collect();
         let rules = comps[0];
-        let password = comps[1];
+        let password: &str = comps[1];
 
         println!("rules: {}", rules);
         println!("password: {}", password);
@@ -18,10 +20,19 @@ fn main()  -> Result<(), Box<dyn std::error::Error>> {
 
         let letter = rule_comps[1];
         let nums: Vec<&str> = rule_comps[0].split("-").collect();
+        let number_low: usize = nums[0].parse().unwrap_or(0);
+        let number_high: usize = nums[1].parse().unwrap_or(0);
 
         println!("Letter: {}", letter);
         println!("Nums: {} {}", nums[0], nums[1]);
+
+        let matches: Vec<&str> = password.matches(letter).collect();
+        if number_low <= matches.iter().count() && matches.iter().count() <= number_high {
+            password_count += 1;
+        } 
     }
+
+    println!("Valid Passwords: {}", password_count);
 
     Ok(())
 }
