14bd08accdf73a6807bc68f75a7590a857c88144
John Ward <john@johnward.net>
2020-12-04 00:09:21 +0000

    Part 2, not quite there yet

--- aoc2020_day3/src/main.rs
+++ aoc2020_day3/src/main.rs
@@ -1,12 +1,13 @@
 use std::env::args;
 use std::result::Result;
 
-fn part1(content: String) -> u32 {
+fn part1(content: &String, right: usize, down: usize) -> u32 {
 
-    let mut string_position = 3;
+    let mut string_position = right;
 
     content.lines()
-        .skip(1)
+        .skip(down)
+        .step_by(down)
         .map(|x| {
             let mut count = 0;
 
@@ -15,8 +16,8 @@ fn part1(content: String) -> u32 {
             } 
 
             // increment the string or wrap it.
-            if string_position + 3 > x.len()-1 {
-                string_position = (string_position + 3) - x.len();
+            if string_position + right > x.len()-1 {
+                string_position = (string_position + right) - x.len();
             }
             else {
                 string_position += 3;
@@ -26,8 +27,13 @@ fn part1(content: String) -> u32 {
         }).sum()
 }
 
-fn part2(content: String) -> u32 {
-    1
+fn part2(content: &String) -> u32 {
+    let mut total = part1(&content, 1, 1);
+    total *= part1(&content, 3, 1);
+    total *= part1(&content, 5, 1);
+    total *= part1(&content, 7, 1);
+    total *= part1(&content, 1, 2);
+    total
 }
 
 
@@ -53,9 +59,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let content = std::fs::read_to_string(&filename)?;
 
-    let part1_answer = part1(content);
+    let part1_answer = part1(&content, 3, 1);
 
     println!("Part1 Answer: {}", part1_answer);
+
+    let part2_answer = part2(&content);
+
+    println!("Part1 Answer: {}", part2_answer);
 
     Ok(())
 }
