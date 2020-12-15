869dc9a08a10e4715aeaf68d60960fd427bd2f29
John Ward <john@johnward.net>
2020-12-15 23:45:44 +0000

    Day 6 Complete

--- aoc2020_day6/src/main.rs
+++ aoc2020_day6/src/main.rs
@@ -1,56 +1,97 @@
+use std::collections::HashMap;
 use std::env::args;
 use std::result::Result;
-use std::collections::HashMap;
 
 fn part1(content: &String) -> usize {
-    customs_forms(content)
+    customs_forms_anyone(content)
 }
 
-// fn part2(passports: &Vec<Passport>) -> u64 {
-//     passports
-//     .iter()
-//     .map(|p| if p.isvalid_strict() { 1 } else { 0 })
-//     .sum()
-// }
+fn part2(content: &String) -> usize {
+    customs_forms_everyone(content)
+}
 
-fn count_questions(forms: &str) -> usize {
-    let mut questions = HashMap::new();
+fn customs_forms_anyone(content: &String) -> usize {
+    let mut questions: HashMap<char, usize> = HashMap::new();
+    let mut count: usize = 0;
 
-    forms.chars().for_each(|s| {
-                questions.insert(s, 1);
+    content.lines().for_each(|s: &str| {
+        if s == "" {
+            count += questions.values().sum::<usize>();
+            questions = HashMap::new();
+        } else {
+            s.chars().filter(|x| x.is_ascii_alphabetic()).for_each(|t| {
+                questions.insert(t, 1);
+                ()
             });
+        }
+    });
 
-    questions.values().sum()
+    count
 }
 
-fn customs_forms(content: &String) -> usize {
-    content.split("\n\n").map(|s: &str| s.lines().map(count_questions).sum::<usize>()).sum::<usize>()
+fn customs_forms_everyone(content: &String) -> usize {
+    let mut questions: HashMap<char, usize> = HashMap::new();
+    let mut count: usize = 0;
+    let mut people: usize = 0;
+
+    content.lines().for_each(|s: &str| {
+        if s == "" {
+            count += questions
+                .values()
+                .map(|v| if *v == people { 1 } else { 0 })
+                .sum::<usize>();
+            questions = HashMap::new();
+            people = 0;
+        } else {
+            s.chars().filter(|x| x.is_ascii_alphabetic()).for_each(|t| {
+                match questions.get(&t) {
+                    Some(v) => questions.insert(t, v + 1),
+                    None => questions.insert(t, 1),
+                };
+
+                ()
+            });
+            people += 1;
+        }
+    });
+
+    count
 }
 
 #[cfg(test)]
 mod test {
-    //use super::*;
+    use super::*;
 
-    const TEST_INPUT: &str = r#"hgt:159cm
-iyr:1997 byr:1994
-hgt:189cm
-ecl:gry pid:564165515
-eyr:2029 hcl:#ceb3a1"#;
+    const TEST_INPUT: &str = r#"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+
+"#;
 
     #[test]
     fn testcase1() {
-        // let content = String::from(TEST_INPUT);
-        // let passports = read_passports(&content);
-        // let count = part1(&passports);
-        // assert_eq!(count, 2);
+        let content = String::from(TEST_INPUT);
+        let part1_answer = part1(&content);
+        assert_eq!(part1_answer, 11);
     }
 
     #[test]
     fn testcase2() {
-        // let content = String::from(TEST_INPUT2);
-        // let passports = read_passports(&content);
-        // let count = part1(&passports);
-        // assert_eq!(count, 5);
+        let content = String::from(TEST_INPUT);
+        let part2_answer = part2(&content);
+        assert_eq!(part2_answer, 6);
     }
 }
 
@@ -61,8 +102,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let part1_answer = part1(&content);
     println!("Part1 Answer: {}", part1_answer);
 
-    // let part2_answer = part2(&passports);
-    // println!("Part2 Answer: {}", part2_answer);
+    let part2_answer = part2(&content);
+    println!("Part2 Answer: {}", part2_answer);
 
     Ok(())
 }
